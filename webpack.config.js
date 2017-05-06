502e54697b634d6c73df3823faf9be5043c8c9c0
David Shaw <david.shaw@skybettingandgaming.com>
2017-05-06 16:50:11 +0100

    sort webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,7 @@ module.exports = {
   entry:'./src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist')    
   },
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, "public")  
-  }
 };
